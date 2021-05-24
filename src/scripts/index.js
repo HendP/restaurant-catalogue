ca80eb40a77a966265fc20245dba32bcbb3fe3b7
HendP <hendpermana12@gmail.com>
2021-05-24 13:36:20 +0700

    add routes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,3 +8,11 @@ const app = new App({
     drawer: document.querySelector('#navigation-drawer'),
     content: document.querySelector('#main-content'),
 });
+
+window.addEventListener('hashchange', () => {
+    app.renderPage();
+});
+
+window.addEventListener('load', () => {
+    app.renderPage();
+});
