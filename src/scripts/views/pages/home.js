92891a2053d6c1c1d6cb20ed805a0eac1c1f94ca
HendP <hendpermana12@gmail.com>
2021-05-24 19:22:57 +0700

    fetch api and display it to home and detail page

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,12 +1,24 @@
+import RestaurantResource from '../../data/restaurant-resource';
+import { createRestaurantItemTemplate } from '../templates/template-creator';
+
 const Home = {
     async render() {
         return `
-        <h2>Home Page</h2>
+        <div class="content">
+            <h2 class="content-heading">Restaurant Catalagoue</h2>
+            <div id="restaurants" class="restaurants">
+            
+            </div>
+        </div>
       `;
     },
 
     async afterRender() {
-
+        const restaurants = await RestaurantResource.restaurantList();
+        const restaurantContainer = document.querySelector('#restaurants');
+        restaurants.forEach((restaurant) => {
+            restaurantContainer.innerHTML += createRestaurantItemTemplate(restaurant);
+        });
     }
 }
 
---
+++ src/scripts/views/templates/template-creator.js
@@ -0,0 +1,35 @@
+import CONFIG from '../../globals/config';
+
+const createRestaurantDetailTemplate = (restaurant) => `
+  <h2 class="restaurant-title">${restaurant.name}</h2>
+  <img class="restaurant-image" src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="${restaurant.name}" />
+  <div class="restaurant-info">
+  <h3>Information</h3>
+    <h4>City</h4>
+    <p>${restaurant.city}</p>
+    <h4>Rating</h4>
+    <p>${restaurant.rating}</p>
+  </div>
+  <div class="restaurant-overview">
+    <h3>Description</h3>
+    <p>${restaurant.description}</p>
+  </div>
+`;
+
+const createRestaurantItemTemplate = (restaurant) => `
+  <div class="restaurant-item">
+    <div class="restaurant-item-header">
+        <img class="restaurant-item-header-image" alt="${restaurant.name}"
+            src="${restaurant.pictureId ? CONFIG.BASE_IMAGE_URL + restaurant.pictureId : 'https://picsum.photos/id/666/800/450?grayscale'}">
+        <div class="restaurant-item-header-rating">
+            <p>⭐️<span class="restaurant-item-header-rating-score">${restaurant.rating}</span></p>
+        </div>
+    </div>
+    <div class="restaurant-item-content">
+        <h3><a href="${`/#/detail/${restaurant.id}`}">${restaurant.name}</a></h3>
+        <p>${restaurant.description}</p>
+    </div>
+  </div>
+  `;
+
+export { createRestaurantDetailTemplate, createRestaurantItemTemplate };
