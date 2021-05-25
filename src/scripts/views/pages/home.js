901e44b6a6c6c81203ff1707571e19ffd3e6481e
HendP <hendpermana12@gmail.com>
2021-05-25 13:58:47 +0700

    add hero image

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -4,6 +4,14 @@ import { createRestaurantItemTemplate } from '../templates/template-creator';
 const Home = {
     async render() {
         return `
+        <div class="hero-image">
+                <div class="hero-text">
+                <h1 class="hero-title">Good food and great vibes</h1>
+                <p class="hero-tagline">
+                    So the only thing we're serious about is food
+                </p>
+                </div>
+        </div>
         <div class="content">
             <h2 class="content-heading">Restaurant Catalagoue</h2>
             <div id="restaurants" class="restaurants">
