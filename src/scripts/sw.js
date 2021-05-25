46c07e482a4a24e369eac9b7a1b136e11e1855a3
HendP <hendpermana12@gmail.com>
2021-05-25 21:55:11 +0700

    add cache

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,18 @@
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
+
+const { assets } = global.serviceWorkerOption;
+
 self.addEventListener('install', (event) => {
     console.log('Installing Service Worker ...');
-
-    // TODO: Caching App Shell Resource
+    event.waitUntil(CacheHelper.cachingAppShell([...assets, './'])); // Caching App Shell Resource
 });
 
 self.addEventListener('activate', (event) => {
     console.log('Activating Service Worker ...');
-
-    // TODO: Delete old caches
+    event.waitUntil(CacheHelper.deleteOldCache()); // Delete old caches
 });
 
 self.addEventListener('fetch', (event) => {
-    console.log(event.request);
-
-    event.respondWith(fetch(event.request));
-    // TODO: Add/get fetch request to/from caches
+    event.respondWith(CacheHelper.revalidateCache(event.request)); // Add/get fetch request to/from caches
 });
