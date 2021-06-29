31b1274b4ae37fe6ac41f9fde0aabf2151b952e9
HendP <hendpermana12@gmail.com>
2021-06-29 20:54:45 +0700

    fixing image responsive heroes and backup image sharp to development stage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.min.css';
-import '../styles/responsive.min.css';
+import '../styles/main.css';
+import '../styles/responsive.css';
 import 'notyf/notyf.min.css';
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
